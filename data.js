b670a266b0692d8517880b8099ce6eaec2257de0
Asaf Gartner <gitcommits@asafgartner.com>
2020-01-18 20:31:42 +0200

    First commit

---
+++ data.js
@@ -0,0 +1,52 @@
+// NOTE: If you want to use back-tick (`) in a description or something you have to escape it (\`)
+var textData = `
+extension: ryansgraphs
+extension_name: Graphs and Stuff
+extension_link: https://github.com/ryanfleury/4coder_fleury
+author: Ryan Fleury
+author_link_twitter: ryanjfleury
+author_link_twitch: ryanfleury
+author_link_website: https://ryanfleury.net
+author_link_github: ryanfleury
+short_desc: Pretty cool extension tbh
+tags: visualization; graphs; parsing; cool;
+long_desc:
+Can do:
+* Line graphs
+* Histograms
+* Animated graphs
+ENDOFDESC
+assets:
+i assets/ryan/graphs/img1.gif
+i assets/ryan/graphs/img2.png
+i assets/ryan/graphs/img3.gif
+v assets/ryan/graphs/vid1.mp4
+ENDOFASSETS
+ENDOFEXTENSION
+extension: ranyspowermode
+extension_name: Power Mode
+extension_link: https://github.com/ryanfleury/4ocder_fleury
+author: Ryan Fleury
+author_link_twitter: ryanjfleury
+author_link_twitch: ryanfleury
+author_link_website: https://ryanfleury.net
+author_link_github: ryanfleury
+short_desc: It's over 9000
+tags: fun;
+assets:
+v assets/ryan/powermode/vid1.mp4
+ENDOFASSETS
+ENDOFEXTENSION
+
+review: ryansgraphs
+type: twitter
+link: serge_rgb/status/1217222033773232128
+ENDOFREVIEW
+review: ryansgraphs
+type: text
+author: Asaf
+avatar: N/A
+date: 2020-01-18
+text: Installation was super easy. Barely an inconvenience.
+ENDOFREVIEW
+`
